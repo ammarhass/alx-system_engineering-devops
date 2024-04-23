c48d47c2a3dccd019fd899d0a5a3b663a325e7b6
ammar <ammar.hass94@gmail.com>
2024-04-23 13:36:15 +0200

    Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""How many subs?"""
+
+
+def number_of_subscribers(subreddit):
+    """queries the Reddit API and returns the number of subscribers"""
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        return 0
+
+    return sub_info.json().get("data").get("subscribers")
